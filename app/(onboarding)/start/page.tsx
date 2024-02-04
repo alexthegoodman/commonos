cdae98e968aabdb371a06998f3ee18327c872334
Alex Goodman <alexthegoodman@gmail.com>
2024-02-03 23:23:43 -0500

    add start page

--- app/(onboarding)/start/page.tsx
+++ app/(onboarding)/start/page.tsx
@@ -1,5 +1,20 @@
 "use client";
 
+import PrimaryPromptForm from "@/components/core/forms/PrimaryPromptForm";
+import { Box, Typography } from "@mui/material";
+
 export default function Start() {
-  return <>Start</>;
+  return (
+    <>
+      <Box display="block" maxWidth="600px" margin="0 auto">
+        <Typography variant="h3" mb={1}>
+          Getting Started
+        </Typography>
+        <Typography variant="body1" mb={2}>
+          Begin your creation journey with a prompt
+        </Typography>
+        <PrimaryPromptForm />
+      </Box>
+    </>
+  );
 }
