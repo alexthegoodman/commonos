cc15d1f8a00aa40d2c66a39bb8b97cc5c119426e
Alex Goodman <alexthegoodman@gmail.com>
2024-10-10 18:44:31 -0400

    type tweak

--- context/SlidesContext.ts
+++ context/SlidesContext.ts
@@ -1,10 +1,35 @@
 import React, { useState, useReducer, Dispatch } from "react";
 
+export interface SlideVisual {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+export interface SlideText extends SlideVisual {
+  id: string;
+  fontFamily: string;
+  fontStyle: string;
+  fontSize: string;
+  lineHeight: number;
+  fill: string;
+  content: string;
+  align: string;
+}
+
+export interface SlideShape extends SlideVisual {
+  id: string;
+  fill: string;
+  sides: number;
+  radius: number;
+}
+
 export interface Slide {
   id: string;
   title: string;
-  texts: any[] | null;
-  shapes: any[] | null;
+  texts: SlideText[] | null;
+  shapes: SlideShape[] | null;
   images: any[] | null;
 }
 
